f805a07cc0302131eca6ad4fee3c7b61905a29c0
ashritha15ns <ashritha15nadaboor@gmail.com>
2023-08-31 10:19:59 +0530

    Files added

---
+++ Main.java
@@ -0,0 +1,114 @@
+ public class Main {
+	public static void main(String[] args) {
+	String WoodenTablename="Ekron";
+	int WoodenTablecost=2625;
+	String WoodenTablecolor="BROWN";
+	boolean IsthisWoodenTableversatile=true;
+
+	System.out.println(WoodenTablename);
+	System.out.println(WoodenTablecost);
+	System.out.println(WoodenTablecolor);
+	System.out.println(IsthisWoodenTableversatile);
+
+	String Toothpastename="Colgate";
+	int Toothpastecost=26;
+	String Toothpastecolor="White";
+	boolean IsthisToothpasteantibacterial=true;
+
+	System.out.println(Toothpastename);
+	System.out.println(Toothpastecost);
+	System.out.println(Toothpastecolor);
+	System.out.println(IsthisToothpasteantibacterial);
+
+	String Handbagname="Gucci";
+	int  Handbagcost=3000;
+	String  Handbagcolor="Peach";
+	boolean IsthisHandbagcarriable=true;
+
+	System.out.println( Handbagname);
+	System.out.println( Handbagcost);
+	System.out.println( Handbagcolor);
+	System.out.println(IsthisHandbagcarriable);
+
+	String Wristwatchname="Fastrack";
+	int   Wristwatchcost=1200;
+	String   Wristwatchcolor="lemonyellow";
+	boolean IsthisWristwatchwaterproof=true;
+
+	System.out.println(Wristwatchname);
+	System.out.println(Wristwatchcost);
+	System.out.println(Wristwatchcolor);
+	System.out.println(IsthisWristwatchwaterproof);
+
+	String Bicyclename="BITWIN";
+	int   Bicyclecost=14000;
+	String Bicyclecolor="red";
+	String Bicycleframe="495 mm";
+	
+	System.out.println(Bicyclename);
+	System.out.println(Bicyclecost);
+	System.out.println(Bicyclecolor);
+	System.out.println(Bicycleframe);
+
+	String Bookname="Wing Of Fire";
+	int   Bookcost=150;
+	String  BookAuthor="APJAbdulKalam";
+	boolean IsitaautobiographyofAPJAbdulKalam=true;
+
+	System.out.println(Bookname);
+	System.out.println(Bookcost);
+	System.out.println(BookAuthor);
+	System.out.println( IsitaautobiographyofAPJAbdulKalam);
+
+	String ElectricTubelightname="Syska";
+	int    ElectricTubelightcost=565;
+	String ElectricTubelightcolor="white";
+	String  ElectricTubelightMaterials="xenon,argon,mercury vapour";
+	
+	System.out.println( ElectricTubelightname);
+	System.out.println( ElectricTubelightcost);
+	System.out.println( ElectricTubelightcolor);
+	System.out.println( ElectricTubelightMaterials);
+
+	String Mousename="Dell";
+	int  Mousecost=756;
+	String  Mousecolor="Black";
+	boolean IsthisMousewireless=false;
+
+	System.out.println(Mousename);
+	System.out.println(Mousecost);
+	System.out.println(Mousecolor);
+	System.out.println( IsthisMousewireless);
+
+	String Bikename="GT 650";
+	long    Bikecost=440000;
+	String Bikecolor="Black";
+	String  BikeMileage="27 km";
+	
+	System.out.println(Bikename);
+	System.out.println( Bikecost);
+	System.out.println( Bikecolor);
+	System.out.println(BikeMileage);
+
+	String Mobilephonename="Redmi 11";
+	long    Mobilephonecost=12000;
+	String Mobilephonecolor="StarBlue";
+	String  MobilephoneStorage="64 GB";
+	
+	System.out.println(Mobilephonename);
+	System.out.println( Mobilephonecost);
+	System.out.println( Mobilephonecolor);
+	System.out.println(MobilephoneStorage);
+
+	
+
+
+
+
+
+	
+
+	
+
+}
+}
